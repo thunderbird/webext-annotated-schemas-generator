6d69d6785e366c3d18b267979dd17f2fa1d2d016
John Bieling <john.bieling@gmx.de>
2025-04-16 17:12:45 +0200

    Revert "[DEBUG] GitHub action may see wrong file names"
    This reverts commit 38679ea571dc65df6f22c6a591266c98659a4572.

--- get_thunderbird_schema_files.js
+++ get_thunderbird_schema_files.js
@@ -477,7 +477,6 @@ async function getJsonFiles(folderPath) {
 
 async function readSchemaFiles(owner, files) {
   for (const file of files) {
-    console.log(path.join(file.path, file.name));
     const json = jsonUtils.parse(
       await fs.readFile(path.join(file.path, file.name), "utf-8")
     );
